1713239f216e5ec0c6ee18543191650bf93a4621
Liu Steven <foreversteve@Lius-MacBook-Pro.local>
2017-11-22 19:39:59 -0500

    Final Changes

--- 04ReverseCharSequence/ReverseCharSequence.java
+++ 04ReverseCharSequence/ReverseCharSequence.java
@@ -1,31 +1,32 @@
 public class ReverseCharSequence implements CharSequence{
+
     String data = "";
     String original;
     public ReverseCharSequence(String input){
-	this.original = input;
-	for (int i = input.length()-1 ;i >=0; i--){
-	    data+= input.substring(i,i+1);
-	}
+	     this.original = input;
+	     for (int i = input.length()-1 ;i >=0; i--){
+	        data+= input.substring(i,i+1);
+	       }
+    }
+    public String toString(){
+      return data;
     }
     public char charAt(int index){
-	return data.charAt(index);
+	     return data.charAt(index);
     }
     public int length(){
-	return data.length();
+	     return data.length();
     }
     public CharSequence subSequence(int start, int end){
-	String l = this.original;
-	CharSequence a = new ReverseCharSequence(l);
-	data = data.substring(start,end);
-	return a;
+	     int l = original.length();
+       String temp = data.substring(start,end);
+	      CharSequence a = new ReverseCharSequence(original.substring(l-end,l-start));
+	       return a;
     }
     public static void main(String[] args){
-	CharSequence lol = new ReverseCharSequence("BLAHblah");
-	System.out.println(lol.data);
-	System.out.println(lol.subSequence(1,3).data);
-    
+	     CharSequence lol = new ReverseCharSequence("BLAHblah");
+	      System.out.println(lol);
+	       System.out.println(lol.subSequence(1,3));
+          System.out.println(lol.charAt(3));
     }
 }
-    
-	
-	
