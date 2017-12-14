159b2a1d27ee7ea01e2e8a006227215dbdf3b6ec
Steven Liu <hliu4@stuy.edu>
2017-12-13 20:11:45 -0500

    Made Final Changes to 09GUI

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -1,50 +1,109 @@
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-public class TemperatureWindow extends JFrame implements ActionListener {
- private Container pane;
+import java.awt.EventQueue;
 
- private JButton b;
- private JLabel l;
-    private JTextField t0,t1;
- private JCheckBox c;
- 
-  //CONSTRUCTOR SETS EVERYTHING UP
-  public TemperatureWindow() {
-     this.setTitle("My first GUI");
-     this.setSize(600,400);
-     this.setLocation(100,100);
-     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
-     pane = this.getContentPane();
-     pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
-    
-     b = new JButton("Convert to Celcius");
-     l = new JLabel("This is AWESOME! (lies)",null,JLabel.CENTER);
-     t0 = new JTextField(50);
-     t1 = new JTextField(50);
-     c = new JCheckBox("Overclock!!!");
-     pane.add(l);
-     pane.add(b);
-     pane.add(t0);
-     pane.add(t1);
-     pane.add(c);
- }
-    public void actionPerformed(ActionEvent e){
-	String event = e.getActionCommand();
-	System.out.println(event);
-	if (event.equals("Convert to Celcius")){
-	        String temp = t0.getText();
-		t1.setText(Double.toString(Temperature.FtoC(Double.parseDouble(temp))));
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TemperatureWindow {
+
+	private JFrame frame;
+	private JTextField t0;
+	private JTextField t1;
+	private JLabel lblFarenheit;
+	private JLabel lblCelcius;
+	private JButton btnNewButton_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TemperatureWindow window = new TemperatureWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-    }
-	
-	
 
-  //MAIN JUST INSTANTIATES + MAKE VISIBLE
-  public static void main(String[] args) {
-     TemperatureWindow g = new TemperatureWindow();
-     g.setVisible(true);
-  }
+	/**
+	 * Create the application.
+	 */
+	public TemperatureWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 606, 437);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		t0 = new JTextField();
+		t0.setBounds(42, 186, 130, 26);
+		frame.getContentPane().add(t0);
+		t0.setColumns(10);
+
+		t1 = new JTextField();
+		t1.setBounds(406, 186, 130, 26);
+		frame.getContentPane().add(t1);
+		t1.setColumns(10);
+
+		JButton btnNewButton = new JButton("Convert to Celcius");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String temp = t0.getText();
+				try {
+					t1.setText(Integer.toString((int)Temperature.FtoC(Double.parseDouble(temp))));
+				}
+				catch( Exception ex) {
+
+				}
+			}
+		});
+		btnNewButton.setBounds(213, 242, 171, 29);
+		frame.getContentPane().add(btnNewButton);
+
+		JLabel lblNewLabel = new JLabel("Temperature Window");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
+		lblNewLabel.setBounds(179, 62, 305, 57);
+		frame.getContentPane().add(lblNewLabel);
+
+		lblFarenheit = new JLabel("Farenheit:");
+		lblFarenheit.setBounds(42, 158, 84, 16);
+		frame.getContentPane().add(lblFarenheit);
+
+		lblCelcius = new JLabel("Celcius:");
+		lblCelcius.setBounds(406, 158, 61, 16);
+		frame.getContentPane().add(lblCelcius);
+
+		btnNewButton_1 = new JButton("Convert to Farenheit");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String temp = t1.getText();
+				try {
+					t0.setText(Integer.toString((int)Temperature.CtoF(Double.parseDouble(temp))));
+				}
+				catch(Exception ex) {
+
+				}
+			}
+		});
+		btnNewButton_1.setBounds(213, 290, 171, 29);
+		frame.getContentPane().add(btnNewButton_1);
+
+
+	}
 }
