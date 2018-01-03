d784860cb3123dd3c501f56dd39027391bdfd82f
Steven Liu <hliu4@stuy.edu>
2018-01-03 02:17:51 -0500

    Renamed ReverseCharSequence.java

--- 04ReverseCharSequence/ReversedCharSequence.java
+++ 04ReverseCharSequence/ReversedCharSequence.java
@@ -1,8 +1,8 @@
-public class ReverseCharSequence implements CharSequence{
+public class ReversedCharSequence implements CharSequence{
 
     String data = "";
     String original;
-    public ReverseCharSequence(String input){
+    public ReversedCharSequence(String input){
 	     this.original = input;
 	     for (int i = input.length()-1 ;i >=0; i--){
 	        data+= input.substring(i,i+1);
@@ -19,12 +19,12 @@ public class ReverseCharSequence implements CharSequence{
     }
     public CharSequence subSequence(int start, int end){
 	     int l = original.length();
-       String temp = data.substring(start,end);
-	      CharSequence a = new ReverseCharSequence(original.substring(l-end,l-start));
+	     String temp = data.substring(start,end);
+	      CharSequence a = new ReversedCharSequence(original.substring(l-end,l-start));
 	       return a;
     }
     public static void main(String[] args){
-	     CharSequence lol = new ReverseCharSequence("BLAHblah");
+	     CharSequence lol = new ReversedCharSequence("BLAHblah");
 	      System.out.println(lol);
 	       System.out.println(lol.subSequence(1,3));
           System.out.println(lol.charAt(3));
