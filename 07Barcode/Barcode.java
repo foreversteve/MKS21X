5fdd7a510e2a767926f33c761d6b98c6ab5c14ff
Steven Liu <hliu4@stuy.edu>
2018-01-04 02:50:19 -0500

    Made minor changes

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -102,6 +102,22 @@ public class Barcode implements Comparable<Barcode>{
 
     		System.out.println(Barcode.toZip("|:::||:::||:::||::||:::||:|:|::|"));
     		System.out.println(Barcode.toCode("11133"));
+
+		System.out.println("Test starts here");
+	      	//Barcode e = new Barcode("asdfd"); //Contains non-barcode characters, should throw IllegalArgumentException
+		//Barcode f = new Barcode("1234"); //Invalid length, should throw IllegalArgumentException
+
+		System.out.println(Barcode.toCode("00294")); // |||:::||:::::|:||:|:::|::|:|:|:|
+		//System.out.println(Barcode.toCode("asdfd")); //Contains non-barcode characters, should throw IllegalArgumentException
+		//System.out.println(Barcode.toCode("1234")); //Invalid length, should throw IllegalArgumentException
+
+		System.out.println(Barcode.toZip("|||:::||:::::|:||:|:::|::|:|:|:|")); //Should return 00294
+		System.out.println(Barcode.toZip("|:::||:::||::|:|:|::|:|:|:::||:")); //Invalid length, should throw IllegalArgumentException
+		System.out.println(Barcode.toZip("|:::||:::||::|:|:|::|:|:|:::||||")); //Incorrect checksum, should throw IllegalArgumentException
+		System.out.println(Barcode.toZip("|a::||:::||::|:|:|::|:|:|:::||:|")); //Contains non-barcode characters, should throw IllegalArgumentException
+		System.out.println(Barcode.toZip("::::||:::||::|:|:|::|:|:|:::||:|")); //First character is not '|', should throw IllegalArgumentException
+		System.out.println(Barcode.toZip("|:::||:::||::|:|:|::|:|:|:::||::")); //Last character is not '|', should throw IllegalArgumentException
+		System.out.println(Barcode.toZip("|::::::::||::|:|:|::|:|:|:::||:|")); //Invalid character sequence, should throw IllegalArgumentException
     }
 
 }
